e3d800e440aa8be154de9e18354f6b8ca344075e
Jackson Luther <gr8peshot@gmail.com>
2020-01-21 20:23:58 -0600

    First Commit

---
+++ ProductRepo.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace CRUD_Operations
+{
+    public class ProductRepo
+    {
+        public static string connString;
+
+        public List<Product> GetProducts()
+        {
+            MySqlConnection conn = new MySqlConnection(connString);
+            using (conn)
+            {
+                conn.Open();
+                MySqlCommand command = conn.CreateCommand();
+
+                command.CommandText = "SELECT ProductID, Name, Price, StockLevel FROM products;";
+                MySqlDataReader reader = command.ExecuteReader();
+
+                var products = new List<Product>();
+                while (reader.Read())
+                {
+                    var row = new Product();
+                    row.ProductID = reader.GetInt32("ProductID");
+                    row.Name = reader.GetString("Name");
+                    row.Price = reader.GetDecimal("Price");
+                    row.StockLevel = reader.GetInt32("StockLevel");
+
+                    products.Add(row);
+                }
+                return products;
+            }
+        }
+
+        public void CreateProduct(Product p)
+        {
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            using (conn)
+            {
+                conn.Open();
+
+                MySqlCommand command = conn.CreateCommand();
+                command.CommandText = "INSERT INTO products (Name, Price, CategoryID, OnSale) " +
+                    "VALUES(@name, @price, @catID, @sale);";
+                command.Parameters.AddWithValue("name", p.Name);
+                command.Parameters.AddWithValue("price", p.Price);
+                command.Parameters.AddWithValue("catID", p.CategoryID);
+                command.Parameters.AddWithValue("sale", p.OnSale);
+
+                command.ExecuteNonQuery();
+            }
+        }
+        public void UpdateProduct(Product p)
+        {
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            using (conn)
+            {
+                conn.Open();
+
+                MySqlCommand command = conn.CreateCommand();
+                command.CommandText = "UPDATE products SET StockLevel=@stock WHERE ProductID=@prodID;";
+                command.Parameters.AddWithValue("stock", p.StockLevel);
+                command.Parameters.AddWithValue("ProdID", p.ProductID);
+                command.ExecuteNonQuery();
+            }
+        }
+        public void DeleteProduct(int id)
+        {
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            using (conn)
+            {
+                conn.Open();
+
+                MySqlCommand command = conn.CreateCommand();
+                command.CommandText = "DELETE FROM products WHERE ProductID=@prodID;";
+                command.Parameters.AddWithValue("prodID", id);
+                command.ExecuteNonQuery();
+
+            }
+        }
+        public void DeleteProduct(string name)
+        {
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            using (conn)
+            {
+                conn.Open();
+
+                MySqlCommand command = conn.CreateCommand();
+                command.CommandText = "DELETE FROM products WHERE name=@name;";
+                command.Parameters.AddWithValue("name", name);
+                command.ExecuteNonQuery();
+
+            }
+
+        }
+        public void DeleteProduct(string name, int id)
+        {
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            using (conn)
+            {
+                conn.Open();
+
+                MySqlCommand command = conn.CreateCommand();
+                command.CommandText = "DELETE FROM products WHERE Name=@name AND ProductID=@prodID;";
+                command.Parameters.AddWithValue("name", name);
+                command.Parameters.AddWithValue("prodID", id);
+                command.ExecuteNonQuery();
+
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace CRUD_Operations
 {
@@ -6,7 +9,65 @@ namespace CRUD_Operations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string defaultKey = File.ReadAllText("appsettings.Debug.Json");
+            JObject jObject = JObject.Parse(defaultKey);
+            JToken token = jObject["DefaultConnection"];
+            string connectionString = token.ToString();
+            ProductRepo.connString = connectionString;
+
+            
+
+
+            ProductRepo repo = new ProductRepo();
+
+            //Create Products
+
+            //Console.WriteLine("Creating Product.....");
+            //var newProduct = new Product
+            //{
+            //    Name = "Jjacksons Product",
+            //    Price = 420.69M,
+            //    CategoryID = 2,
+            //    OnSale = 0
+            //};
+            //repo.CreateProduct(newProduct);
+            //Console.WriteLine("Product Created!");
+
+            //Update Products
+
+            //Console.WriteLine("Updating Product....");
+            //var newInfo = new Product { StockLevel = 27, ProductID = 941 };
+            //repo.UpdateProduct(newInfo);
+            //Console.WriteLine("Product Updated!");
+            
+            // DELETE
+
+            //repo.DeleteProduct(943);
+            //repo.DeleteProduct("Jacksons Product");
+            //repo.DeleteProduct("Jjacksons Product", 946);
+
+            // Read Products
+
+            List<Product> products = repo.GetProducts();
+
+            foreach (var prod in products)
+            {
+                Console.WriteLine($"{prod.ProductID}  {prod.Name} ----------- ${prod.Price}--------Quantity: {prod.StockLevel} of these items.");
+            }
+
+            
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
